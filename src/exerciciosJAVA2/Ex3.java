b2643642ccdeb1c21889bd1aaa4a61ded8e13a73
Guilherme Teixo <guilhermesantosjp@gmail.com>
2023-07-05 18:19:46 -0300

    5/8

--- src/exerciciosJAVA2/Ex3.java
+++ src/exerciciosJAVA2/Ex3.java
@@ -10,19 +10,29 @@ public static void main (String[]args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int horaInicial = sc.nextInt();
-		int horaFinal = sc.nextInt();
+		int codigo = sc.nextInt();
+		int quantidade = sc.nextInt();
 		
-		int duracao;
+		double total;
 		
-		if (horaInicial < horaFinal) {
-			duracao = horaFinal - horaInicial;
-		} else {
-			duracao = 24 - horaInicial + horaFinal;
+		if (codigo == 1) {
+			total = quantidade * 4.0;
+		} 
+		else if (codigo == 2) {
+			total = quantidade * 4.5;
+		} 
+		else if (codigo == 3) {
+			total = quantidade * 5.0;
+		}
+		else if (codigo == 4) {
+			total = quantidade * 2.0;
+		}
+		else {
+			total = quantidade * 1.5;
 		}
 		
-		System.out.println("O JOGO DUROU "+duracao+" HORA(S)");
-		
+		System.out.printf("Total: R$ %.2f%n", total);
+				
 		sc.close();
 	}
 }
