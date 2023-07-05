bca087411cfee965bd019dd7f1b8f6cf8b5a1049
Guilherme Teixo <guilhermesantosjp@gmail.com>
2023-07-05 17:47:29 -0300

    4/8

--- src/exerciciosJAVA2/Ex3.java
+++ src/exerciciosJAVA2/Ex3.java
@@ -10,16 +10,18 @@ public static void main (String[]args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-				
-		if (A%B == 0){
-			System.out.println("SAO MUTIPLOS");
-		} else if (B%A == 0){
-			System.out.println("SAO MUTIPLOS");
+		int horaInicial = sc.nextInt();
+		int horaFinal = sc.nextInt();
+		
+		int duracao;
+		
+		if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
 		} else {
-			System.out.println("NAO SAO MUTIPLOS");
+			duracao = 24 - horaInicial + horaFinal;
 		}
+		
+		System.out.println("O JOGO DUROU "+duracao+" HORA(S)");
 		
 		sc.close();
 	}
---
+++ src/exerciciosJAVA2/Ex4.java
@@ -0,0 +1,26 @@
+package exerciciosJAVA2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex4 {
+	
+public static void main (String[]args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+				
+		if (A%B == 0){
+			System.out.println("SAO MUTIPLOS");
+		} else if (B%A == 0){
+			System.out.println("SAO MUTIPLOS");
+		} else {
+			System.out.println("NAO SAO MUTIPLOS");
+		}
+		
+		sc.close();
+	}
+}
