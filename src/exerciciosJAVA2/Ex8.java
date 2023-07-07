4ca3d842255b5c44642296d3370d51ce857044c0
Guilherme Teixo <guilhermesantosjp@gmail.com>
2023-07-07 18:16:07 -0300

    8/8

--- src/exerciciosJAVA2/Ex8.java
+++ src/exerciciosJAVA2/Ex8.java
@@ -9,27 +9,30 @@ public static void main (String[]args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		double salario = sc.nextDouble();
-		double porcentagem;
-		double abatimento;
-		double imposto;
 		
-		if (salario == 0.00 && salario <= 2000.00) {
+		double imposto;
+		if (salario <= 2000.0) {
+			imposto = 0.0;
+		}
+		else if (salario <= 3000.0) {
+			imposto = (salario - 2000.0) * 0.08;
+		}
+		else if (salario <= 4500.0) {
+			imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+		}
+		else {
+			imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+		}
+
+		if (imposto == 0.0) {
 			System.out.println("Isento");
 		}
-		else if (salario >= 2000.01 && salario <= 3000.00) {
-			porcentagem = 8;
-			imposto = (salario/100)*porcentagem;
-			System.out.printf("R$ %.2f", imposto);
+		else {
+			System.out.printf("R$ %.2f%n", imposto);
 		}
-		else if (salario >= 3000.01 && salario <= 4500.00) {
-			abatimento = salario - 1000;
-			porcentagem = 8;
-			imposto = (abatimento/100)*porcentagem;
-			System.out.printf("R$ %.2f", imposto);
-		}
-				
+		
 		sc.close();
 	}
 }
